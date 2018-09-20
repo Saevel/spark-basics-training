7b60432a0722da294be3e231c98721e049662ae9
klok <Kamil.Owczarek@gft.com>
2018-09-20 11:08:48 +0200

    Solutions for Task 1

--- src/main/scala/prv/saevel/trainings/spark/basics/task1/BlacklistFilter.scala
+++ src/main/scala/prv/saevel/trainings/spark/basics/task1/BlacklistFilter.scala
@@ -5,5 +5,16 @@ import prv.saevel.trainings.spark.basics.Customer
 
 object BlacklistFilter {
 
-  def apply(allCustomers: RDD[String], blacklist: RDD[String]): RDD[Customer] = ???
+  def apply(allCustomers: RDD[String], blacklist: RDD[String]): RDD[Customer] = {
+    allCustomers
+      .map(fromCsv)
+      .subtract(blacklist.map(fromCsv))
+      .filter(_.age < 30)
+      .filter(_.accountBalance > 15000)
+  }
+
+  private def fromCsv(line: String): Customer = {
+    val splitLine = line.split(", ")
+    Customer(splitLine(0).toLong, splitLine(1), splitLine(2), splitLine(3).toInt, splitLine(4).toDouble)
+  }
 }
