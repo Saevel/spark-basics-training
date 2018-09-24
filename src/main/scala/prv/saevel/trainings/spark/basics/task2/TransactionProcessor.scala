e2340d0732708ba55516d9e2b5bddb67190179fb
klok <Kamil.Owczarek@gft.com>
2018-09-24 14:50:44 +0200

    Daily commit

--- src/main/scala/prv/saevel/trainings/spark/basics/task2/TransactionProcessor.scala
+++ src/main/scala/prv/saevel/trainings/spark/basics/task2/TransactionProcessor.scala
@@ -23,24 +23,29 @@ object TransactionProcessor {
       .filter(_.size >= 3)
       .map(split => Transaction(split(0).toLong, split(1).toLong, split(2).toDouble))
       // INFO: You could add some custom partitioning here to optimize :)
-      .groupBy(_.accountId)
 
-    val suspiciousAccounts = accounts.keyBy(_.id).leftOuterJoin(transactions).filter{ case (_, (account, optionalTransactions)) =>
+    val groupedTransactions = transactions.groupBy(_.accountId)
+
+    val suspiciousAccounts = accounts.keyBy(_.id).leftOuterJoin(groupedTransactions).filter{ case (_, (account, optionalTransactions)) =>
       optionalTransactions.fold(account.balance != 0.0)(transactions =>
-        transactions.map(_.amount).fold(0.0)(_ + _) != account.balance
+        transactions.map(_.amount).sum != account.balance
       )
     }.map {case(_, (account, _)) => account}.cache
 
     val debitedCustomerIds = accounts.subtract(suspiciousAccounts)
       .groupBy(_.customerId)
-      .mapValues(_.map(_.balance).fold(0.0)(_ + _))
+      .mapValues(_.map(_.balance).sum)
       .filter{ case (_, balance) => balance < 0.0}
-      .map { case (customerId, _) => customerId}
+      .map { case (customerId, _) => customerId.toString}
 
+    /*
     val suspiciousCustomerIds = customers.join(suspiciousAccounts.keyBy(_.customerId))
       .cache
       .map{ case (_, (customer, _)) => customer.id.toString}
       .distinct
+      */
+
+    val suspiciousCustomerIds = suspiciousAccounts.map(_.customerId).distinct
 
     debitedCustomerIds.saveAsTextFile(config.customersWithDebitFile)
 
